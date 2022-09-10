aa7766b9ea319ae224fb5e59b397f540194d39d1
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2022-09-10 16:39:59 -0300

    an example of using localDate.now()

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-//if a I wanna import some class or librarie outside the package, must do the proper import:
-//import java.util.library
+
 package ej1;
 import java.time.LocalDate;
 
@@ -8,14 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		Persona pedro = new Persona();
 		
-		//how do we call an object? always following the next syntax: object.message() where the message is a method
 		System.out.println(pedro.getDni());
 		System.out.println(pedro.getBodyMassIndex());
 		
-		//never do this!
-		//pedro.height = 24;
-		//never! always call the method, for example: pedro.setHeight(24);
-		//why? because with the method I have absolute control on how the attr may be changed from outside, adding the logic
-		//to manage what comes from parameter-args
+		LocalDate hoy = LocalDate.now();
+		System.out.println("hoy es: " +hoy);
+
 	}
 }
