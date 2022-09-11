9137d21f73328010325b3a45a2b635af8e7c3cb4
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2022-09-11 20:44:51 -0300

    set up for the exercise with constructors, getters and setters

---
+++ Electrodomestico.java
@@ -0,0 +1,63 @@
+package ej2;
+
+public class Electrodomestico {
+	
+	//attr
+	private String name;
+	private double mainprice;
+	private String color;
+	private double energyConsume;
+	private int weight;
+	//constructors
+	public Electrodomestico (String name) {
+		this(name, 100);
+	}
+	public Electrodomestico (String name, double mprice) {
+		this(name, mprice, "gris plata");
+	}
+	public Electrodomestico (String name, double mprice, String color) {
+		this(name, mprice, color, 10);
+	}
+	public Electrodomestico (String name, double mprice, String color, double eCons) {
+		this(name, mprice, color, eCons, 2);
+	}
+	public Electrodomestico (String name, double mprice, String color, double eCons, int w) {
+		this.setName(name);
+		this.setMainprice(mprice);
+		this.setColor(color);
+		this.setEnergyConsume(eCons);
+		this.setWeight(w);
+	}
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getMainprice() {
+		return mainprice;
+	}
+	public void setMainprice(double mainprice) {
+		this.mainprice = mainprice;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getEnergyConsume() {
+		return energyConsume;
+	}
+	public void setEnergyConsume(double energyConsume) {
+		this.energyConsume = energyConsume;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+}
